b52f992a8984a3c3e8d4ad3231672f4d01c2739d
Rody Castillo <rody.castillo.tacuri@gmail.com>
2020-12-08 14:57:11 -0500

    second commit

---
+++ 8-kyu/function-17.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/58e43389acfd3e81d5000a88/train/javascript
+
+//Solution: Nº01
+
+// function circleCircumference(circle) {
+//   var circumference = (circle.radius *2*Math.PI)
+//   return circumference
+//   }
